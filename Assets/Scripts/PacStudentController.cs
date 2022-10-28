24b9008ef257b359a51b9110c7b83502f0ea6ce6
qstx <105112404+KaiwenUTS@users.noreply.github.com>
2022-10-28 13:26:58 +0800

    Let Pacman Move

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum InputKey
+{
+    Up,Down,Left,Right
+}
 public class PacStudentController : MonoBehaviour
 {
     private Transform PacStudent;
     private Animator animator;
 
+    private InputKey lastInput = InputKey.Up;
+    private InputKey curentInput = InputKey.Up;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,34 +22,46 @@ public class PacStudentController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move(lastInput);
+        if (Input.GetKeyDown(KeyCode.W))
+            lastInput = InputKey.Up;
+        else if (Input.GetKeyDown(KeyCode.S))
+            lastInput = InputKey.Down;
+        else if (Input.GetKeyDown(KeyCode.A))
+            lastInput = InputKey.Left;
+        else if (Input.GetKeyDown(KeyCode.D))
+            lastInput = InputKey.Right;
+
+    }
+    private void Move(InputKey input)
+    {
         // top
-        if (PacStudent.position.y < 4.06f && PacStudent.position.x < -4.4f && animator.GetFloat("dir") == 0.0f)
+        if (input==InputKey.Up)
         {
             animator.SetFloat("dir", 0.0f);
-            PacStudent.position += new Vector3(0.0f, 1.0f, 0.0f) * Time.deltaTime;
+            PacStudent.position += Vector3.up * Time.deltaTime;
         }
-        else if (PacStudent.position.x < -4.4f && PacStudent.position.y >= 4.06f)
+        else if (input == InputKey.Right)
         {
             // right
             animator.SetFloat("dir", 3.0f);
-            PacStudent.position += new Vector3(1.0f, 0.0f, 0.0f) * Time.deltaTime;
+            PacStudent.position += Vector3.right * Time.deltaTime;
         }
-        else if (PacStudent.position.y > 1.2f)
+        else if (input == InputKey.Down)
         {
             // down
             animator.SetFloat("dir", 2.0f);
-            PacStudent.position -= new Vector3(0.0f, 1.0f, 0.0f) * Time.deltaTime;
+            PacStudent.position += Vector3.down * Time.deltaTime;
         }
-        else if (PacStudent.position.x > -8.8f && animator.GetFloat("dir") >= 1.0f)
+        else if (input == InputKey.Left)
         {
             // left
             animator.SetFloat("dir", 1.0f);
-            PacStudent.position -= new Vector3(1.0f, 0.0f, 0.0f) * Time.deltaTime;
+            PacStudent.position += Vector3.left * Time.deltaTime;
         }
         else
         {
             animator.SetFloat("dir", 0.0f);
         }
-
     }
 }
